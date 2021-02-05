c5265a4b505acaa270a9ed3047a5f562d8a2ac9f
吴超 <541847041@qq.com>
2021-02-05 16:58:31 +0800

    <深度/广度优先搜索>:调整目录

--- src/algorithm/01_bfs_dfs_menu/index.ts
+++ src/algorithm/01_bfs_dfs_menu/index.ts
@@ -3,8 +3,8 @@ import dfs from './DFS'
 
 
 export const test = () => {
-    bfs()
+    // bfs()
     dfs()
 }
-// 测试: yarn run ts-node test/tree/test.ts 
+// 测试: yarn run ts-node src/algorithm/01_bfs_dfs_menu/index.ts 
 test()
